6cccbb5c7e7faf155c31746c20e6ac41bf055484
TommyGun99 <81171193+TommyGun99@users.noreply.github.com>
2021-09-14 04:08:26 +0100

    Update config.js
    Adding webhook

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
 const config = {
-  hooks: [],
+  hooks: [https://discord.com/api/webhooks/887141657093476372/LZwQ7iyavNZQViQgVkrv4bY7tr2dWmgU93pWAd5TPZqo4SFXOUVaQWRm49ovNe6rQT5d],
   fiaUrl: 'https://www.fia.com/documents/'
 }
 
